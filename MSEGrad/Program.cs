247418a078789cdc0b2c384f6a55f8a888eb5d43
kaister3 <wyk0401@icloud.com>
2020-07-29 14:23:17 +0800

    First time

---
+++ MSEGrad/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace MSEGrad
+{
+    //利用矩阵运算的梯度下降法，求得 HuberLoss 的极值
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var matrix1 = new DenseMatrix(2, 3);
+            Console.WriteLine(matrix1);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
